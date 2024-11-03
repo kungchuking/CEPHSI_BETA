2c351c775dec457aa26e51b2b53f8eeefbd16355
Chu King Kung <kungchu@seth.eecg>
2024-11-03 12:49:50 -0500

    Added the ability to save checkpoints/images for inspection.

--- train.py
+++ train.py
@@ -5,6 +5,14 @@ import loss
 import matplotlib.pyplot as plt
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from tqdm import tqdm
+import os
+
+# -- Essential directories (Create them if they don't already exist)
+ess_dirs = ["./results/", "./checkpoints/"]
+
+for d in ess_dirs:
+    if not os.path.exists(d):
+        os.makedirs(d)
 
 fd = open("train.log", "w")
 
@@ -17,9 +25,17 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.backends.cudnn.benchmark = True
 
 n_rpt      = 1000 # -- Number of iterations before reporting the loss, PSNR, and other metrics 
-n_epoch    = 1000 # -- Total number of epochs
-n_img      = 10   # -- Number of epochs before saving images for inspection
+n_epoch    = 5000 # -- Total number of epochs
+n_img      = 100  # -- Number of epochs before saving images for inspection
 patch_size = [512, 512]
+
+# -- SOme metrics to help evaluate the performance of the model.
+# -- The PSNRs of the last epoch
+train_psnr_o = -1.
+valid_psnr_o = -1.
+# -- The PSNRs of the current epoch
+train_psnr = 0.
+valid_psnr = 0.
 
 # -- The Model
 model = cep_system(sigma_range=[0, 1e-12],
@@ -113,13 +129,14 @@ for epoch in range(n_epoch):
             print ("[INFO] Iteration {}; Loss: {}; PSNR: {};".format(i, run_loss / float(n_rpt), run_psnr_avg / float(i+1)), file=fd)
             run_loss = .0
 
-    print ("[INFO] Training Set PSNR: {};".format(run_psnr_avg / float(i+1)), file=fd)
+    train_psnr = run_psnr_avg / float(i+1)
+    print ("[INFO] Training Set PSNR: {};".format(train_psnr), file=fd)
 
     # -- Save some output images for inspectin once every n_img iterations
     if epoch % n_img == 0 and epoch != 0:
         for n in range(output.shape[1]):
             plt.imshow(output[0, n, 0, ...].detach().numpy(), cmap="gray")
-            plt.savefig("./results/epoch_{}_n_{}.png".format(epoch, n))
+            plt.savefig("./results/train_epoch_{}_n_{}.png".format(epoch, n))
 
     """
     print ("[INFO] ce_blur.shape: ", ce_blur.shape)
@@ -155,5 +172,19 @@ for epoch in range(n_epoch):
             run_psnr_avg += psnr_avg
 
         # -- Report the performance on the validation set
-        print ("[INFO] Validation Set PSNR: {};".format(run_psnr_avg / float(i+1)), file=fd)
+        valid_psnr = run_psnr_avg / float(i+1)
+        print ("[INFO] Validation Set PSNR: {};".format(valid_psnr), file=fd)
+
     fd.flush()
+
+    # -- Save some output images for inspection once every n_img iterations
+    if epoch % n_img == 0 and epoch != 0:
+        for n in range(output.shape[1]):
+            plt.imshow(output[0, n, 0, ...].detach().numpy(), cmap="gray")
+            plt.savefig("./results/valid_epoch_{}_n_{}.png".format(epoch, n))
+
+    # -- Save the parameters as checkpoints if they yield higher PSNRs than those of previous epochs.
+    if train_psnr > train_psnr_o and valid_psnr > valid_psnr_o:
+        train_psnr_o = train_psnr
+        valid_psnr_o = valid_psnr
+        torch.save(model.state_dict(), "./checkpoints/epoch_{}_valid_psnr_{}_train_psnr_{}.pth".format(epoch, valid_psnr, train_psnr))
